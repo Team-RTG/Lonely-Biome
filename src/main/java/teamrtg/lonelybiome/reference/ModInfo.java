6d8ee93b951ed90ff78684b6e5c32f5faf9415f8
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-01-19 00:03:48 +0000

    Updated to 1.10.2

--- src/main/java/teamrtg/lonelybiome/reference/ModInfo.java
+++ src/main/java/teamrtg/lonelybiome/reference/ModInfo.java
@@ -1,11 +1,15 @@
 package teamrtg.lonelybiome.reference;
 
+
 public class ModInfo
 {
     public static final String MOD_ID = "lonelybiome";
     public static final String MOD_NAME = "Lonely Biome";
-    public static final String MOD_VERSION = "@VERSION@";
-    
-	public static final String PROXY_COMMON = "teamrtg.lonelybiome.proxy.CommonProxy";
-	public static final String PROXY_CLIENT = "teamrtg.lonelybiome.proxy.ClientProxy";
+    public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String MCF_MINVER = "0.0-MCF+MINVER";
+    public static final String MCF_MAXVER = "9001.0-MCF+MAXVER";
+    public static final String MOD_DEPS = ";after:MODDEPS";
+    public static final String CONFIG_DIRECTORY = MOD_ID;
+    public static final String PROXY_COMMON = "teamrtg.lonelybiome.proxy.CommonProxy";
+    public static final String PROXY_CLIENT = "teamrtg.lonelybiome.proxy.ClientProxy";
 }
